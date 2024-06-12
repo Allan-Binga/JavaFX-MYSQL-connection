566d6728b2ba63f80e4806fb19b435af6287b5db
Allan-Binga <allanbinga73@gmail.com>
2024-06-11 23:14:49 -0400

    Login and Icon(Second Commit)

--- src/main/java/com/example/loginfx/Main.java
+++ src/main/java/com/example/loginfx/Main.java
@@ -3,6 +3,7 @@ package com.example.loginfx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,11 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("registration.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
-        stage.setTitle("Hello!");
+
+        Image icon = new Image(Main.class.getResourceAsStream("/com/example/loginfx/icon.jpg"));
+
+        stage.getIcons().add(icon);
+        stage.setTitle("Login System");
         stage.setScene(scene);
         stage.show();
     }
